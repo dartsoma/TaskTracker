19006e02add61549768707af408290b7feba9086
Chad-soma Foster <chadsomaf@gmail.com>
2025-08-26 22:20:41 -0400

    Adding Json Files, Current Date and Time, Scalable

--- task-cli.cpp
+++ task-cli.cpp
@@ -1,26 +1,78 @@
 #include <iostream>
 #include <fstream>
 #include <nlohmann/json.hpp>
+#include <ctime>
+#include <chrono>
+#include <format>
+
+
 using namespace std;
 using json = nlohmann::json;
 
-int main (int argc, char** argv) {
-
-   /*
-     if (argc > 1) {
-
-        jsonFile == argv[2];
-    }
-    */
-    string jsonFile = "tasks.json";
-
-    std::ifstream file(jsonFile);
-    json data = json::parse(file);
-
-    json jsonNew;
 
 
+json tasksArray = json::array();
+json task = json::object();
+int taskNum;
 
-    cout << data.dump() << endl;
+//
+// typedef struct Task {
+//     int id;
+//     char* description;
+//     char* status;
+//     char* createdAt;
+//     char* updatedAt;
+// };
 
+
+string getTime() {
+    auto t = chrono::system_clock::now();
+    time_t tp =chrono::system_clock::to_time_t(t);
+    string s = ctime(&tp);
+    s.pop_back();
+    return s;
 }
+
+void addTask(string taskDesc = "") {
+    tasksArray.push_back({
+     {"Id", tasksArray.size()},
+     {"Description", taskDesc},
+     {"Status", "todo"},
+     {"CreatedAt", getTime()},
+     {"UpdatedAt", ""}
+ });
+}
+
+    int main (int argc, char** argv) {
+
+        string jsonFile = "taskss.json";
+        std::ifstream file(jsonFile);
+
+        if (file.good()) {
+            json data = json::parse(file);
+        } else {
+            json jsonNew;
+            // jsonNew["Tasks"].push_back({
+            //     {"ID", 0},
+            //     {"Description", ""},
+            //     {"Status", ""},
+            //     {"CreatedAt", ""},
+            //     {"UpdatedAt", ""}
+            // });
+            // jsonNew["Tasks"].push_back( {
+            //     {"ID", 1},
+            //     {"Description", ""},
+            //     {"Status", ""},
+            //     {"CreatedAt", ""},
+            //     {"UpdatedAt", ""}
+            // });
+
+            addTask();
+            jsonNew = tasksArray;
+
+            ofstream jsonOut("taskDefault.json");
+
+            jsonOut << jsonNew.dump(4);
+            cout << jsonNew.dump(4) << endl;
+        }
+    }
